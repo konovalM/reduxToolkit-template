eeedc1b0e22836e458d02d6e8b2309fa8725dd6d
konovalM <konovalenko.ii4elka@mail.ru>
2023-01-13 11:09:46 +0300

    [feat] fuck eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import {setupStore} from "./store/store";
-import {Provider} from "react-redux";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { setupStore } from "./store/store";
+import { Provider } from "react-redux";
 
 const store = setupStore();
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
@@ -16,4 +16,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
--- src/store/reducers/ActionCreators.ts
+++ src/store/reducers/ActionCreators.ts
@@ -2,13 +2,16 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { IUser } from "../../models/IUser";
 
-export const fetchUsers = createAsyncThunk("user/fetchAll", async (_, thunkAPI) => {
-  try {
-    const response = await axios.get<IUser[]>(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-    return response.data;
-  } catch (e: any) {
-    return thunkAPI.rejectWithValue(e.message)
+export const fetchUsers = createAsyncThunk(
+  "user/fetchAll",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get<IUser[]>(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      return response.data;
+    } catch (e: any) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
   }
-});
+);
